5437ff9d03294df3a95cf148332c18754404f300
Luciano Minuzzi <luciano.toto@gmail.com>
2018-09-12 12:39:06 -0300

    Criacao da primeira versao

---
+++ Fatorial.java
@@ -0,0 +1,13 @@
+public class Fatorial {
+    public static void main(String[] args) {
+        System.out.println(calcularFatorial(5));
+    }
+
+    private static int calcularFatorial(int n) {
+        int resultado = 1;
+        for(int i = 2; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
